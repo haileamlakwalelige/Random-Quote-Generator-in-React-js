244b3b4426957dad5383699872f67605eae9ecbc
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-03-07 16:14:32 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends React.Component{
+  state={ advice: ""}
+
+
+  componentDidMount(){
+    this.fetchAdvice();
+  }
+
+  fetchAdvice  =()=>{
+    axios.get("https://api.adviceslip.com/advice")
+    .then(response =>{
+      const advice=response.data.slip.advice;
+      this.setState({advice :advice});
+    })
+    .catch(err=>{
+      console.log(err);
+    });
+
+  }
+
+  render(){
+    const {advice}=this.state;
+    return(
+      <div className='app'>
+        <div className='card'>
+        <h1 className='heading'>{advice}</h1>
+         <button onClick={this.fetchAdvice} className='button'>
+         <span>Get Advice</span></button>
+        </div>
+      </div>
+    
+    );
+  }
+}
+
+
+
+export default App;
